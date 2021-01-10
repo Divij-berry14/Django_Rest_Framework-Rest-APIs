7d2682f6630932fe3023245464c92dad03e62c9d
Divij <divij.berry1999@gmail.com>
2021-01-10 20:59:37 +0530

    DRF

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path,include
+from django.urls import path, include
 from snippets import views
+from snippets.views import *
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     # path('snippets/',views.SnippetList.as_view()),
@@ -10,4 +11,8 @@ urlpatterns = [
     # path('login/',views.LoginView.as_view()),
     # path('logout/',views.LogoutView.as_view()),
     # path("res/",views.res,name="res")
+    path('poll/', views.poll),
+    path('poll/<int:id>/', views.poll_details),
+    path('pollAPI/', PollAPIView.as_view()),
+    path('pollAPI/<int:id>/', Poll_detailsAPI.as_view())
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404, HttpRequest, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
@@ -6,6 +6,9 @@ from snippets.serializers import *
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
@@ -107,5 +110,83 @@ from rest_framework.authentication import TokenAuthentication
 # def res(request):
 #     return HttpResponse("Welcome user")
 
+@csrf_exempt
+def poll(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        json_parser = JSONParser()
+        data = json_parser.parse(request)
+        serializer = QuestionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
+@csrf_exempt
+def poll_details(request, id):
+    try:
+        instance = Question.objects.get(id=id)
+        print(instance)
+    except Question.DoesnotExist as e:
+        return JsonResponse({"error": "Given Question does not found"}, status=400)
+
+    if request.method == 'GET':
+        serializer = QuestionSerializer(instance)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        json_parser = JSONParser()
+        data = json_parser.parse(request)
+        serializer = QuestionSerializer(instance, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        instance.delete()
+        return JsonResponse(status=204)
+
+class PollAPIView(APIView):
+    def get(self, request):
+        questions = Question.objects.all()   #Queryset
+        print(questions)
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data,status=200)
+    def post(self,request):
+        data = request.data
+        serializer = QuestionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors, status=400)
+
+class Poll_detailsAPI(APIView):
+    def get_object(self, id):
+        try:
+            return Question.objects.get(id=id)
+        except Question.DoesnotExist as e:
+            return Response({'error': "Given Question not found"}, status=400)
+    def get(self, request, id):
+        instance = self.get_object(id)
+        print(instance)
+        serializer = QuestionSerializer(instance)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        data = request.data
+        instance = self.get_object(id)
+        serializer = QuestionSerializer(instance,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, id):
+        instance = self.get_object(id)
+        instance.delete()
+        return Response(status=204)
 
